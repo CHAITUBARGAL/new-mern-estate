56ee3cc217e46c876b7f15b591f5e6c13bf666a6
Chetan Bargal <chetan.bargal11@gmail.com>
2023-10-13 12:26:32 +0530

    add reducx toolkit

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { FaSearch } from 'react-icons/fa';
+// import { FaSearch } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
 export default function Header() {
@@ -17,7 +17,7 @@ export default function Header() {
             placeholder='Search...'
             className='bg-transparent focus:outline-none w-24 sm:w-64'
           />
-          <FaSearch className='text-slate-600' />
+          {/* <FaSearch className='text-slate-600' /> */}
         </form>
         <ul className='flex gap-4'>
           <Link to='/'>
