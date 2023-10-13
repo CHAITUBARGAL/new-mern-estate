5da28419ab7228b7a9a56d0859f2c8871a5c4d57
Chetan Bargal <chetan.bargal11@gmail.com>
2023-10-13 17:52:31 +0530

    create OAuth components

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -14,7 +14,11 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true,
-    }
+    } ,
+    avatar:{
+        type: String,
+        default: "https://lh3.googleusercontent.com/a/ACg8ocKW3i6ABypYvE5o4zfq2Db6KfO1aA67kCbykDkWi8QYxg=s96-c"
+      },
 }, { timestamps: true });
 
 const User = mongoose.model('User', userSchema);
