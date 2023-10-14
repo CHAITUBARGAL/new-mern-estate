826fb5b162bf9360f4efab3a3d88c947ce30a958
CHAITUBARGAL <chetan.bargal11@gmail.com>
2023-10-14 10:21:19 +0530

    errror solve insignup page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import SignIn from './pages/Signin';
-import SignUp from './pages/Signup';
+
 import About from './pages/About';
 import Profile from './pages/Profile';
 import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
+import SignUp from './pages/Signup';
 
 export default function App() {
   return (
